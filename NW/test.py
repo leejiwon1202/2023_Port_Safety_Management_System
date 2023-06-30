12ca437bd5bfb9ce573bd49e69c3fa525af4dae8
sjmama <ykh45sko@gmail.com>
2023-06-30 11:04:02 +0900

    soul

--- NW/test.py
+++ NW/test.py
@@ -1,11 +1,15 @@
 import cv2 as cv
 import numpy as np
 import threading
+import os
 #import torch 
 
 
+
 fps= 30
-dst='./test.mp4'    #저장 위치
+dst='test.mp4'    #저장 위치
+
+
 
 save_second=3   #몇초 저장할건지
 
@@ -18,30 +22,23 @@ def imagemodel(model, cap):
     result=model(cap)
 
 def showNsave():
-    print("11111")
-    cap = cv.VideoCapture('rtmp://34.216.5.166:1935/live/0000')
+    cap = cv.VideoCapture('rtmp://15.164.216.116:1935/live/0000')
     ret, frame = cap.read()
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
-    out = cv.VideoWriter(dst, fourcc, fps, (frame.shape[0], frame.shape[1]))
-    print("22222")
+    out = cv.VideoWriter(dst, fourcc, fps, (frame.shape[1], frame.shape[0]))
     while cap.isOpened():
-        print("3333")
         ret, frame = cap.read()
-        print('4444')
         # if frame is read correctly ret is True
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
         cv.imshow('frame', frame)
-        print("5555")
         queue.append(frame)
-        print(type(frame))
         if len(queue) > queue_size:
             queue.pop(0)
-            
-        if cv.waitKey(1) == ord('q'):
+        if cv.waitKey(1) == ord('q'):#여기는 시그널 처리 해줘야함
             break
-        if cv.waitKey(1) == ord('s'):
+        if cv.waitKey(1) == ord('s'):#여기는 위험 신호 감지해서 저장하게 해야하고
             for f in queue:
                 out.write(f)
         print(len(queue))
@@ -51,6 +48,8 @@ def showNsave():
 S_S=threading.Thread(target=showNsave, args=())#show and save
 
 def main():
+    if os.path.exists(dst):
+        os.remove(dst)
     S_S.start()
     
     cv.destroyAllWindows()
