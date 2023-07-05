8737c4483132eea6289b6a2abdbd3f56a771fd0b
sjmama <ykh45sko@gmail.com>
2023-07-05 21:38:00 +0900

    event control

--- NW/test.py
+++ NW/test.py
@@ -2,14 +2,13 @@ import cv2 as cv
 import numpy as np
 import threading
 import os
+import time
 #import torch 
-
-
 
 fps= 30
 dst='test.mp4'    #저장 위치
 
-
+event_flag=0
 
 save_second=3   #몇초 저장할건지
 
@@ -18,12 +17,20 @@ queue = []
 
 #model=model()
 #model.load_state_dict(torch.load(./aaaa))
-def imagemodel(model, cap):
-    result=model(cap)
+def flagcontrol():
+    global event_flag
+    while True:
+        event_flag=0
+        time.sleep(5)
+        event_flag=1
+        time.sleep(5)
+        
 
 def showNsave():
-    cap = cv.VideoCapture('rtmp://15.164.216.116:1935/live/0000')
+    cap = cv.VideoCapture('rtmp://3.35.25.173:1935/live/0000')
     ret, frame = cap.read()
+    innerflag=0
+    print(type(frame))
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
     out = cv.VideoWriter(dst, fourcc, fps, (frame.shape[1], frame.shape[0]))
     while cap.isOpened():
@@ -38,16 +45,23 @@ def showNsave():
             queue.pop(0)
         if cv.waitKey(1) == ord('q'):#여기는 시그널 처리 해줘야함
             break
-        if cv.waitKey(1) == ord('s'):#여기는 위험 신호 감지해서 저장하게 해야하고
+        if innerflag==0 and event_flag==1:#여기는 위험 신호 감지해서 저장하게 해야하고
+            innerflag=event_flag
             for f in queue:
                 out.write(f)
-        print(len(queue))
+        elif innerflag==1 and event_flag==1:
+            out.write(frame)
+        elif innerflag==1 and event_flag==0:
+            innerflag=event_flag
+        print(event_flag)
     out.release()
     cap.release()
 
 S_S=threading.Thread(target=showNsave, args=())#show and save
-
+fctl=threading.Thread(target=flagcontrol, args=())
+fctl.daemon=True
 def main():
+    fctl.start()
     if os.path.exists(dst):
         os.remove(dst)
     S_S.start()
